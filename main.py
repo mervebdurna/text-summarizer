5a8558c8ecf627d5792bc72fd6b976cb285b0c94
mervebdurna <mervebdurna@gmail.com>
2023-08-28 17:35:48 +0530

    complete data ingestion module

---
+++ main.py
@@ -0,0 +1,14 @@
+from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from textSummarizer.logging import logger
+
+STAGE_NAME  = "Data Ingestion"
+
+try:
+    logger.info(f">>>>>>>> Stage {STAGE_NAME} started <<<<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>>>> Stage {STAGE_NAME} completed <<<<<<<<")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
