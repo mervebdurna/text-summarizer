685be9394e362648c0d8bebfcc1817114f9176d0
mervebdurna <mervebdurna@gmail.com>
2023-08-29 17:01:12 +0530

    completed data_validation module

--- main.py
+++ main.py
@@ -12,3 +12,5 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
--- src/textSummarizer/entity/__init__.py
+++ src/textSummarizer/entity/__init__.py
@@ -7,3 +7,10 @@ class DataIngestionConfig:
     source_URL: str
     local_data_file: Path
     unzip_dir: Path
+
+
+@data_class(frozen=True)
+class DataValidationConfig:
+    root_dir: Path
+    STATUS_FILE: str
+    ALL_REQUIRED_FILE: list
