133e52000d9388ee80577c6b112366e972e367ab
mervebdurna <mervebdurna@gmail.com>
2023-09-01 16:26:47 +0530

    completed transformation and model trainin components

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from textSummarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from textSummarizer.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 from textSummarizer.logging import logger
 
 STAGE_NAME  = "Data Ingestion"
@@ -40,6 +41,22 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+STAGE_NAME  = "Model Trainer"
+
+try:
+    logger.info(f">>>>>>>> Stage {STAGE_NAME} started <<<<<<<<")
+    model_trainer = ModelTrainerTrainingPipeline()
+    model_trainer.main()
+    logger.info(f">>>>>>>> Stage {STAGE_NAME} completed <<<<<<<<")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+
 
 
 
